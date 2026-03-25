fae43d925fe496a37f86e1b63c969806d40ab93c
Simone Orru <simone.orru@secomind.com>
2026-03-25 14:31:44 +0100

    Try to activate split layout

--- keyboards/stupidkeyboard/keymaps/default/keymap.c
+++ keyboards/stupidkeyboard/keymaps/default/keymap.c
@@ -10,17 +10,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * │  A │  S │  D │  F │  G │  │  │  H │  J │  K │  L │  ; │  │
      * │  Z │  X │  C │  V │  B │  │  │  N │  M │  , │  . │  / │  │
      * └─────────┌────┼────┼────┤  │  ├────┼────┼────┐────────────┘
-     * │ ESC│ SPC│ TAB│  │  │ ENT│ BSP│ DEL│
-     * └────┴────┴────┘  │  └────┴────┴────┘
+     *           │ ESC│ SPC│ TAB│  │  │ ENT│ BSP│ DEL│
+     *           └────┴────┴────┘  │  └────┴────┴────┘
      */
     [0] = LAYOUT_split_3x5_3(
         // Left hand
         KC_A,    KC_B,    KC_C,    KC_D,    KC_E,
         KC_F,    KC_G,    KC_H,    KC_I,    KC_L,
         KC_M,    KC_N,    KC_O,    KC_P,    KC_Q,
-        KC_R,    KC_S,    KC_T
+        KC_R,    KC_S,    KC_T,
 
         // Right hand
+        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,
+        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
+        KC_U,    KC_V,    KC_W,    KC_X,    KC_Y,
+        KC_Z,    KC_MINUS,    KC_EQUAL
     )
 };
 
